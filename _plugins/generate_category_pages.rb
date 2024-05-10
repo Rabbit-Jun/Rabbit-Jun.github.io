3a4b30296039af485400d688f2b39aa05806e362
SongJunEon <junun24@gmail.com>
2024-05-10 11:16:20 +0900

    post 카테고리 수정 및 포스트와 사이드바 구성 비율 조정pc

--- _plugins/generate_category_pages.rb
+++ _plugins/generate_category_pages.rb
@@ -8,12 +8,15 @@ Jekyll::Hooks.register :site, :post_write do |site|
     path = File.join(dir, "#{slugified_category}.html")
     unless File.exist?(path)
       File.open(path, 'w') do |file|
-        file.write("---\nlayout: default\ntitle: #{category}\npermalink: /category-#{slugified_category}/\n---\n")
+        file.write("---\nlayout: default\ntitle: #{category}\npermalink: /category/#{slugified_category}/\n---\n")
+        file.write("<div class=\"posts\">\n")
         file.write("{% for post in site.categories['#{category}'] %}\n")
-        file.write("<div class=\"post\">\n")
+        file.write("<div class=\"post-header\">\n")
         file.write("  <h2><a href=\"{{ post.url }}\">{{ post.title }}</a></h2>\n")
+        file.write("  <p class=\"post-date\">{{ post.date | date: '%b %-d, %Y' }}</p>\n")
         file.write("</div>\n")
         file.write("{% endfor %}\n")
+        file.write("</div>\n")
       end
     end
   end
